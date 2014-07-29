857e22d38cf57d94d76cec364f6dfc015e469859
Michael L Perry <michael@qedcode.com>
2014-07-29 11:04:51 -0500

    Initialize application with components.

---
+++ IoCSample/IoCSample/Application.cs
@@ -0,0 +1,23 @@
+﻿using IoCSample.Framework;
+using IoCSample.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoCSample
+{
+    public class Application
+    {
+        private SignUpController _signUpController;
+
+        public void Init()
+        {
+            DatabaseConnection connection = new DatabaseConnection("connectionstring");
+            ISignUpService signUpService = new SignUpService(connection);
+            SignUpController controller = new SignUpController(signUpService);
+            _signUpController = controller;
+        }
+    }
+}
